941f5b60bc3d5e0304c436fe2d93ad9e3cb6659d
Yaman <husoyaman1987@gmail.com>
2021-07-20 16:23:24 +0300

    To Do Lisr

--- To Do List App/5/script.js
+++ To Do List App/5/script.js
@@ -4,11 +4,15 @@ const deleteAll = document.querySelector(".delete-btn");// .delete-btn classına
 
 // Add task
 addBtn.addEventListener("click", function () {          // + BUTONUNA basıldığında 
+    if(!input.value){
+        alert("Please enter a task")
+    }
     const li = document.createElement("li");            // Bir adet li etiketi üretip onu li değişkenine atadık
     li.classList.add("task-item");                      // Bu li etiketinin classına "task-item ekledik"
-    li.innerHTML = input.value + '<button class="delete-item"><i class="fas fa-trash-alt"></i></button>'; // Takiben bu li etiketinin içeriğine de hem input tan gelecek değeri hem de bir adet çöp kutusu iconu ekledik
+    li.innerHTML = input.value + '<div><button class="completed-item"><i class="far fa-check-circle"></i></button>    <button class="delete-item"><i class="fas fa-trash-alt"></i></button></div>'; // Takiben bu li etiketinin içeriğine de hem input tan gelecek değeri hem de bir adet çöp kutusu iconu ekledik
     document.querySelector("ul").appendChild(li);       // Sonra HTML deki ul listesini yakaladık ve yukarıdaki li etiketini onun child olarak ekledik
     input.value = "";                                   // Takiben input içerisini resetledik
+
 });
 
 // Delete all task
@@ -21,6 +25,9 @@ deleteAll.addEventListener("click", function (event) {  // "Delete All" BUTONUNA
 const ul = document.querySelector("ul");                // ul listesini ul değişkeninde yakaladık
 ul.addEventListener("click", (event) => {               // ul etiketi üzerine tıklandığında
     if (event.target.className === "fas fa-trash-alt") { // Eğer kullanıcının tık hedefinin classı "fas fa-trash-alt" ise, (yani çöp kutusu)
-        event.target.parentElement.parentElement.classList.add("delete-task"); // Kullanıcının tıkladığının parentının(button) parantının(li) classına "delete-task" classını ekle ki
+        document.querySelector("li").remove(); // Kullanıcının tıkladığının parentının(button) parantının(li) classına "delete-task" classını ekle ki
     }                                                                          // css de yazılmış olan üstü çizilme uygulansın
+    if (event.target.className === "far fa-check-circle"){
+        document.querySelector("li").classList.add("checked");
+    }
 });
